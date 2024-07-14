6c6caa9ea0d1d75c1342c83c89f5a3a30ed04788
ysldennis <nashdennys@gmail.com>
2024-07-14 23:40:57 +0300

    assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2 ;
+
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+const max = 20;
